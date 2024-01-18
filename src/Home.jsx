1642f4325345d492f6e0226259586c039c034cc6
euna0104 <koeuna0104@naver.com>
2024-01-18 14:43:11 +0900

    style: Home.jsx css 수정

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const Home = () => {
+  const StyledDiv = styled.div`
+    .p-lg-8 {
+        padding: 3.5rem!important;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    .row {
+        --bs-gutter-x: 2rem;
+        --bs-gutter-y: 0;
+        display: flex;
+        flex-wrap: wrap;
+        margin-left: calc(var(--bs-gutter-x)*-.5);
+        margin-right: calc(var(--bs-gutter-x)*-.5);
+        margin-top: calc(var(--bs-gutter-y)*-1);
+    }
+    .align-items-center {
+        align-items: center!important;
+    }
+    .min-vh-100 {
+        min-height: 100vh!important;
+    }
+    .d-flex {
+        display: flex!important;
+    }
+    .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
+        margin-top: 0;
+        margin-bottom: .5rem;
+        font-weight: 550;
+        line-height: 1.2;
+        color: var(--bs-heading-color);
+    }
+    .bg-home {
+        --bs-bg-opacity: 1;
+        background-color: rgb(38,31,68);
+    }
+    .btn-primary {
+        background-color: #ff5938;
+        border-color: #ff5938;
+        color: #fff;
+    }
+    .btn {
+        border: 1px solid transparent;
+        border-radius: .25rem;
+        display: inline-block;
+        font-size: 1rem;
+        font-weight: 600;
+        line-height: 1.6;
+        padding: .6rem 1rem;
+        text-align: center;
+        text-decoration: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        user-select: none;
+        vertical-align: middle;
+    }
+  `;
+
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const accessToken = sessionStorage.getItem('accessToken');
@@ -41,110 +99,46 @@ const Home = () => {
         alert('로그인이 필요합니다.');
     }
   };
-  const handleKutis = () => {
-      window.location.href = 'https://kutis.kyonggi.ac.kr/webkutis/view/indexWeb.jsp';
-   };
-  const handleLMS = () => {
-    window.location.href = 'https://lms.kyonggi.ac.kr/login.php';
-  };
-  const handleKGU = () => {
-      window.location.href = 'https://www.kyonggi.ac.kr/www/index.do';
-   };
-   const handleAIHOME = () => {
-       window.location.href = 'http://cs.kyonggi.ac.kr:8080/Index';
-   };
-   const handleSWUNIV = () => {
-       window.location.href = 'https://swuniv.kyonggi.ac.kr/';
-    };
 
   return (
-    <>
-      {/* header */}
-      <div className="header position-sticky border-3 border-top border-primary bg-dark">
-        {/* navigation start */}
+    <StyledDiv>
+      <div className="min-vh-100 d-flex align-items-center" style={{ backgroundImage: 'url(/img/test.jpg)',
+        backgroundSize: 'cover' }}>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-default">
-            <a className="navbar-brand" onClick={handleHome}>
-              <img src="/img/cspop_logo.png" width="110" height="40" alt="" />
-            </a>
-            <ul className="navbar-nav ms-auto me-lg-3">
-            </ul>
-            <div className="header-btn">
-              {isLoggedIn ? (
-                <button className="btn btn-primary" type="button" onClick={handleLogout} id="submit-button">
-              	  Logout
-              	</button>
-              ) : (
-                <button className="btn btn-primary" type="button" onClick={handleLogin} id="submit-button">
-                  Login
-                </button>
-              )}
+              <div className="row">
+                <div className="offset-lg-3 col-lg-6 col-12">
+                  <a href="/api/home" className="mb-4 d-flex justify-content-center">
+                    <img src="/img/cspop_logo.png" width="160" height="55" alt="" />
+                  </a>
+                  <div className="bg-home p-4 p-lg-8 rounded-3">
+                    <form>
+                      <h2 className="mb-3 text-white font-weight-bold">경기대학교 졸업시스템</h2>
+                      <div className="mb-3">
+                        <label htmlFor="email" className="form-label text-white-50">
+                          경기대학교 컴퓨터공학부 학생들을 위한 졸업시스템입니다.
+                        </label>
+                      </div>
+                      <br />
+                      <div className="d-grid">
+                        <button className="btn btn-primary" type="submit" onClick={handleNotice}>
+                          Graduation Start
+                        </button>
+                      </div>
+                      <div className="d-lg-flex justify-content-between mt-4 mb-3">
+                        <p className="text-muted font-14">
+                          <a href="signup.html">Sign up</a>
+                        </p>
+                        <p className="font-14">
+                          <a href="password-reset.html">Forget Password</a>
+                        </p>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-          </nav>
         </div>
-        {/* navigation close */}
-      </div>
-
-      {/* pageheader section */}
-      <div className="bg-shape bg-dark">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12 col-lg-12 col-md-12 col-12"></div>
-          </div>
-        </div>
-      </div>
-
-      {/* dashboard nav */}
-      <div className="py-12">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-8 col-md-12 col-12">
-              &nbsp;
-              <h1 className="fw-bold mb-3">경기대학교 졸업 시스템</h1>
-              <p className="mb-4">
-                당신의 졸업 요건을 확인해 보세요.
-              </p>
-              <button className="btn btn-success" onClick={handleNotice}>
-                Graduation Start
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* footer */}
-      <div className="footer pt-11 pb-3 bg-dark text-body">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-2 col-md-4 col-6">
-              <div className="mb-4" onClick={handleKutis}>
-                <h4 className="mb-4 text-white">Kutis</h4>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-4 col-6">
-              <div className="mb-4" onClick={handleLMS}>
-                <h4 className="mb-4 text-white">LMS</h4>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-4 col-6">
-              <div className="mb-4" onClick={handleKGU}>
-                <h4 className="mb-4 text-white">KGU</h4>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-4 col-6">
-              <div className="mb-4" onClick={handleAIHOME}>
-                <h4 className="mb-4 text-white">AI-HOME</h4>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-4 col-6">
-              <div className="mb-4" onClick={handleSWUNIV}>
-                <h4 className="mb-4 text-white">SW-UNIV</h4>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </StyledDiv>
   );
 };
 
