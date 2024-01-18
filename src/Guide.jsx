c605d322ac6adfbce2dfe279957831977e78e6be
euna0104 <koeuna0104@naver.com>
2024-01-18 17:31:10 +0900

    style: Guide.jsx 둥근부분추가

--- src/Guide.jsx
+++ src/Guide.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Header from './Common/Header';
+import styled from 'styled-components';
 
 const Guide = ({ userId }) => {
   const [data, setData] = useState({ text: '', id: '' });
@@ -16,60 +17,55 @@ const Guide = ({ userId }) => {
       });
   }, []);
 
-  return (
-    <div>
-      <Header />
-      <div className="bg-shape bg-secondary">
-        <div className="container">
-          <div className="row">
-            <div className="offset-xl-1 col-xl-10 col-lg-12 col-md-12 col-12">
-              <div className="pt-lg-18 pb-lg-16 py-12">
-                <div className="row align-items-center">
-                 <div>&nbsp;</div>
-                  <div className="col-lg-6 col-md-6 col-12">
-                    <h1 className="h2 text-white mb-2">안내 및 내규</h1>
-                    <div>&nbsp;</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  const StyledDiv = styled.div`
+    .bg-dark {
+      --bs-bg-opacity: 1;
+      background-color: rgb(38,31,68) !important;
+    }
+    .bg-shape {
+      position: relative;
+      height: 200px;
+    }
+    .bg-shape:after {
+      background:url(/img/curve-shape.svg);
+      background-position-x:center;
+      background-repeat:no-repeat;
+      background-size:cover;
+      bottom:-15px;content:"";
+      height:62px;
+      position:absolute;
+      width:100%
+    }
+  `;
 
-      <div className="pb-10 mt-n10">
-        <div className="container">
-          <div className="row">
-            <div className="offset-lg-1 col-lg-10 col-md-12 col-12">
-              <div className="card">
-                <div className="card-body p-4 p-lg-7">
-                  <div>
-                    <div className="card table-responsive">
-                      <div className="card-body">
-                        <div className="row mt-2">
-                          <div className="col-12">
-                            <div className="card-text" style={{ height: '400px' }}>
-                              {data.text}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="d-flex justify-content-end">
-                    {userId && userId.includes('admin') && (
-                      <Link to={`modifyGuide/${data.id}`} className="btn btn-primary text-white float-right">
-                        수정
-                      </Link>
-                    )}
-                  </div>
+  return (
+    <StyledDiv>
+      <div>
+        <Header />
+        <div className="bg-shape bg-dark">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-12 col-lg-12 col-md-12 col-12">
+                <div className="py-lg-14 py-12 text-center mb-4" style={{ padding: '50px 0' }}>
+                  <h4 className="text-white display-4 mb-3">안내 및 내규</h4>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="py-12 ">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 col-md-12 col-12">
+                <h1 className="fw-bold mb-3">This is a H1, Perfect's for titles.</h1>
+                <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Stress, for the United States element ante. Duis cursus, mi quis viverra ornare, eros pain, sometimes none at all, freedom of the living creature was as the profit and financial
+                  security. Jasmine neck adapter and just running it lorem makeup sad smile of the television set.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </StyledDiv>
   );
 };
 
