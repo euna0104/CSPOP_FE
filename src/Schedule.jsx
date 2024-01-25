4120d4c21ab5a36c6f9ba62ff8a183473a70ac90
euna0104 <koeuna0104@naver.com>
2024-01-25 15:29:50 +0900

    style: Schedule.jsx head 추가

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -10,6 +10,28 @@ const StyledDiv = styled.div`
     text-align: center;
     margin-left: 190px;
   }
+  .bg-dark {
+    --bs-bg-opacity: 1;
+    background-color: rgb(38,31,68) !important;
+  }
+  .bg-shape {
+    position: relative;
+    height: 200px;
+  }
+  .bg-shape:after {
+    background:url(/img/curve-shape.svg);
+    background-position-x:center;
+    background-repeat:no-repeat;
+    background-size:cover;
+    bottom:-15px;content:"";
+    height:62px;
+    position:absolute;
+    width:100%
+  }
+  .title {
+    margin-right: 190px;
+    font-family: "NanumGothicBold";
+  }
 `;
 
 const Schedule = () => {
@@ -17,6 +39,17 @@ const Schedule = () => {
     <StyledDiv>
       <div>
         <Header />
+        <div className="bg-shape bg-dark">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-12 col-lg-12 col-md-12 col-12">
+                <div className="py-lg-14 py-12 text-center mb-4" style={{ padding: '50px 0' }}>
+                  <h1 className="text-white display-4 mb-3 title">진행일정</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="pb-12 mt-lg-n18 mt-n10">
           <div className="container">
             <div className="row">
