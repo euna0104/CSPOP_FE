aab82dbecd9d181746dd6d3565cd7ff308be8f95
euna0104 <koeuna0104@naver.com>
2024-01-25 15:13:56 +0900

    style: Schedule.jsx 표 margin설정

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -1,22 +1,74 @@
-import React, { useState } from 'react';
-import Header from './Common/Header'; // Header 컴포넌트를 import
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Header from './Common/Header';
+
+const StyledDiv = styled.div`
+  font-family: 'NanumGothic';
+
+  .row {
+    text-align: center;
+    margin-left: 190px;
+  }
+`;
 
 const Schedule = () => {
   return (
-    <div>
-      <Header />
-
-      {/* 배경 */}
-      <div className="bg-shape bg-secondary">
-        <div className="container">
-          <div className="row">
-            <div className="offset-xl-1 col-xl-10 col-lg-12 col-md-12 col-12">
-              <div className="pt-lg-18 pb-lg-16 py-12 ">
-                <div className="row align-items-center">
-                 <div>&nbsp;</div>
-                  <div className="col-lg-6 col-md-6 col-12">
-                    <h1 className="h2 text-white mb-2">진행일정</h1>
-                    <div>&nbsp;</div>
+    <StyledDiv>
+      <div>
+        <Header />
+        <div className="pb-12 mt-lg-n18 mt-n10">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-9 col-md-8 col-12">
+                <div className="card rounded-3 mb-4">
+                  <div className="table-responsive border-0">
+                    <table className="table mb-0 text-nowrap">
+                      <thead className="bg-light">
+                        <tr>
+                          <th scope="col" className="py-2 border-bottom-0">
+                            단계
+                          </th>
+                          <th scope="col" className="py-2 border-bottom-0">
+                            시작일정
+                          </th>
+                          <th scope="col" className="py-2 border-bottom-0">
+                            종료일정
+                          </th>
+                          <th scope="col" className="py-2 border-bottom-0">
+                            상태
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td className="align-middle">e5BZptmrBl</td>
+                          <td className="align-middle">2021-12-07</td>
+                          <td className="align-middle">
+                            American Express ending in 1234
+                          </td>
+                          <td className="align-middle">$5.00</td>
+                          <td className="align-middle">
+                            <a href="#">
+                              <i className="fe fe-download"></i>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td className="align-middle">4aS1taQR4F</td>
+                          <td className="align-middle">2021-11-07</td>
+                          <td className="align-middle">
+                            American Express ending in 1234
+                          </td>
+                          <td className="align-middle">$5.00</td>
+                          <td className="align-middle">
+                            <a href="#">
+                              <i className="fe fe-download"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
@@ -24,55 +76,7 @@ const Schedule = () => {
           </div>
         </div>
       </div>
-
-      {/* 내용 */}
-      <div className="pb-10 mt-n10">
-        <div className="container">
-          <div className="row">
-            <div className="offset-lg-1 col-lg-10 col-md-12 col-12">
-              <div className="card">
-                <div className="card-body p-4 p-lg-7">
-                  <div>
-                    <div className="card table-responsive">
-                      <table id="table" className="table mb-0" style={{ borderColor: 'black' }}>
-                        <thead className="table-dark">
-                          <tr style={{ textAlign: 'center' }}>
-                            <th data-field="step">
-                              <div className="th-inner sortable both">단계</div>
-                            </th>
-                            <th data-field="start_date">
-                              <div className="th-inner sortable both">시작 일정</div>
-                            </th>
-                            <th data-field="final_date">
-                              <div className="th-inner sortable both">종료 일정</div>
-                            </th>
-                            <th data-field="state">
-                              <div className="th-inner sortable both">상태</div>
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {/*}{schedules.map((data, index) => (
-                            <tr key={index} style={{ textAlign: 'center' }}>
-                              <td>{data.step.stepToString}</td>
-                              <td>{data.startDate}</td>
-                              <td>{data.endDate}</td>
-                              <td>{data.scheduleState.scheduleStateToString}</td>
-                            </tr>
-                          ))}*/}
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                  {/* 추가 */}
-                </div>
-              </div>
-              {/* 추가 */}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </StyledDiv>
   );
 };
 
